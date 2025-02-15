fd7904e0791ea2fd5fbee83983b3d1d1ed371b41
Jesus Sanchez <josusssanher@gmail.com>
2025-02-15 16:30:39 -0600

    archivos reparados

---
+++ wonky_app/src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+import { Button } from 'react-bootstrap';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+        <Button variant="primary" size="lg">
+          Large button
+        </Button>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
